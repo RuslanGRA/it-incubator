9a80096738c71c41b24cac90cf92bb0eb3de3550
Ruslan Gaitarov <online0180@mail.ru>
2025-08-08 15:57:14 +0300

    изменения задачи 2 урока 2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6);
-let dice2 = Math.floor(Math.random() * 6);
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
 let isWinningDouble = (dice1 === dice2) && (dice1 > 3);
 
 console.log('Первый бросок: ' + dice1)
