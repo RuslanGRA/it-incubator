60fd765c354fe8cea27b88050aae321f18cef8b3
Ruslan Gaitarov <online0180@mail.ru>
2025-08-11 13:24:17 +0300

    изменения 2\2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 7) + 1;// your code
-let dice2 = Math.floor(Math.random() * 7) + 1;// your code
+let dice1 = Math.floor(Math.random() * 7);// your code
+let dice2 = Math.floor(Math.random() * 7);// your code
 let isWinningDouble = (dice1 === dice2) && (dice1 > 3);// your code
 
 console.log('Первый бросок: ' + dice1)
