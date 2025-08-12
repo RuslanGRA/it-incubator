81d2bd2e4e1cccb15ce1f33379e03ef92d21a140
Ruslan Gaitarov <online0180@mail.ru>
2025-08-12 16:32:52 +0300

    изменения

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,10 +11,10 @@ let dice1 = Math.floor(Math.random() * 6) + 1;// your code
 let dice2 = Math.floor(Math.random() * 6) + 1;// your code
 let isWinningDouble = dice1 === dice2 && dice1 > 3;// your code
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-    console.log('Выигрышный дубль!');
-} else {
-    console.log('Не выигрышный дубль.');
-} 
+// console.log('Первый бросок: ' + dice1)
+// console.log('Второй бросок: ' + dice2)
+// if (isWinningDouble) {
+//     console.log('Выигрышный дубль!');
+// } else {
+//     console.log('Не выигрышный дубль.');
+// } 
